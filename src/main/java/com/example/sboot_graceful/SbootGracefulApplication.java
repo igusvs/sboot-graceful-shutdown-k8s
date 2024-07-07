d25157514e5872a1b66c735db297e90ef17948f2
igusvs <igoralves159@gmail.com>
2024-07-07 12:46:09 -0300

    commit inicial

---
+++ src/main/java/com/example/sboot_graceful/SbootGracefulApplication.java
@@ -0,0 +1,13 @@
+package com.example.sboot_graceful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootGracefulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootGracefulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sboot_graceful/SbootGracefulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sboot_graceful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootGracefulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
