bd8f31006fd6d87aad7df946124ff17568f0f653
igusvs <igoralves159@gmail.com>
2024-07-09 13:00:30 -0300

    configure sqs listener

--- src/main/java/com/example/sboot_graceful/GracefulShutdownEndpoint.java
+++ src/main/java/com/example/sboot_graceful/GracefulShutdownEndpoint.java
@@ -17,8 +17,6 @@ public class GracefulShutdownEndpoint {
 
     @WriteOperation
     public void shutdown() {
-
-
         System.out.println("teste");
     }
 
--- src/main/java/com/example/sboot_graceful/QueueConsumer.java
+++ src/main/java/com/example/sboot_graceful/QueueConsumer.java
@@ -1,18 +1,14 @@
 package com.example.sboot_graceful;
 
-
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class QueueConsumer {
-/*
-
 
     @SqsListener("queue-teste-shutdown")
     public void listener(MessageTeste message){
         System.out.println("Mensagem recebido : " + message.nome());
     }
-*/
 
 }
